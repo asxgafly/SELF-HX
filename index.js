3ff3a2c46967885c736386222760db0f9999ac06
angga <92718848+asxgafly@users.noreply.github.com>
2021-12-12 20:40:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const { jadibot, stopjadibot, listjadibot } = require('./lib/jadibot')
 
 banChats = true
 offline = false
-targetpc = '6285751056816'
-owner = '6285751056816'
-fake = 'HEXAGONZ'
+targetpc = '6287741166552'
+owner = '6287741166552'
+fake = 'hlly'
 numbernye = '0'
 waktu = '-'
 alasan = '-'
