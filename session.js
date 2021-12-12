177995fa5de633df2504c4081df6b96a85f9c947
angga <92718848+asxgafly@users.noreply.github.com>
2021-12-12 20:44:58 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "IZHCeyebalXvFVOnerrXWg==",
+	"serverToken": "1@x3wCnIWMvepQd4JiHTpvNNxuIofmFQXKgnXgYTiqxRZNzdHiOz4omLSwwztIHPa4EXtMdO2n0YkFZg==",
+	"clientToken": "O+fUxQYhauBbCpR7Do4mjb2yErjxpIs7Tv+NwAfWkE8=",
+	"encKey": "gUv837vLaNztFkHh/XMbqvZM4dA1DpgM7eRrTRlx2Ok=",
+	"macKey": "1LXMmwTT3uurcvZfjxK8B4gDt3fGHu/rtfxhiPgkFDQ="
+}
